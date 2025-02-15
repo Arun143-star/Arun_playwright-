371e29f1e2f0b37bde96dbb13ed4df28e93da1d7
Arun143-star <Arunrithu2007@gmail.com>
2025-02-15 21:51:09 +0530

    updated

--- tests/rithu.spec.js
+++ tests/rithu.spec.js
@@ -1,3 +1,5 @@
 senioro 1: i am  very sry for the inncident  rithu ma ;
-senioro 2:could u give me a second chance for ou relationship;
-senioro 3 :this is last  rithu :
+senioro 2: could u give me a second chance for ou relationship;
+senioro 3 :this is last  rithu ;
+senioro 4: sry sry sry sry sry sry sry sry sry sry sry sry sry sry;
+
