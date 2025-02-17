7af2fec5974d0f3b6cdf511876937d210f7fa4db
Arun143-star <Arunrithu2007@gmail.com>
2025-02-17 09:53:59 +0530

    new

--- tests/latenight.spec.js
+++ tests/latenight.spec.js
@@ -4,13 +4,12 @@ test('latenight',async({page})=>{
     await page.waitForTimeout(3000);
     await page.locator("//input[@type='text']").fill("watch");
     await page.locator("//input[@type='submit']");
-    await page.waitForTimeout(3000);
-    await page.click("//input[@type='submit']");
-    await page.click("//img[@class='s-image']");
-    await page.waitForTimeout(3000);
-    await page.waitForSelector("//input[@id='add-to-cart-button']").click();
-    await page.locator("//a[@href='/cart?ref_=sw_gtc']").click();
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(6000);
+    await page.locator("//img[@class='s-image']").click();
+
+    
+
+
     
 
 
